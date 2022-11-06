368beff4ae5621d12ecffb2e9ed74c021bd38496
nicole-esquer <nicole.esquer5@gmail.com>
2022-11-05 19:54:04 -0700

    remove save_and_open for circleci

--- spec/features/charity_dashboard/index_spec.rb
+++ spec/features/charity_dashboard/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Charity Dashboard Index' do
   it 'has a button to choose animal charities' do
     visit charities_path
     expect(page).to have_button('Animal Charities')
-    
+
     click_on 'Animal Charities'
     expect(current_path).to eq(charity_index_path)
     expect(page).to have_content('Animal Charities')
@@ -12,10 +12,9 @@ RSpec.describe 'Charity Dashboard Index' do
   it 'has a button to choose wildlife charities' do
     visit charities_path
     expect(page).to have_button('Wildlife Charities')
-    
+
     click_on 'Wildlife Charities'
     expect(current_path).to eq(charity_index_path)
-    save_and_open_page
     expect(page).to have_content('Wildlife Charities')
   end
 end
