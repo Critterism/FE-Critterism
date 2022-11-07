6b8df0a5f82127ea2c55d484d8459485249c4e92
Anna Marie Sterling <sterling.am@gmail.com>
2022-11-07 10:49:17 -0700

    Trivia answers and current_question

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/google_oauth2/callback', to: 'sessions#google'
 
-  resources :user, only: [:create, :show]
+  # resources :user, only: [:create, :show]
   resources :trivia, only: %i[index]
 
   controller :sessions do
---
+++ spec/facades/trivia_facade_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe TriviaFacade do
+  describe '.create_trivia', :vcr do
+    it 'retrieves data for 10 trivia questions' do
+      trivia = TriviaFacade.create_trivia
+
+      expect(trivia).to be_an Array
+      expect(trivia.count).to eq(10)
+
+      trivia.each do |section|
+        expect(section.current_question).to be_a Hash
+        expect(section.current_question.keys).to eq([:category, :type, :difficulty, :question, :correct_answer, :incorrect_answers, :answers])
+        expect(section).to be_a Trivia
+        expect(section.category).to eq('Animals')
+        expect(section.answers).to be_an Array
+        expect(section.correct_answer).to be_a String
+        expect(section.difficulty).to be_a String
+        expect(section.incorrect_answers).to be_an Array
+        expect(section.question).to be_a String
+        expect(section.type).to be_a String
+      end
+    end
+  end
+end
