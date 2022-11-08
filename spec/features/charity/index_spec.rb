dcbc320db789a484d0d8ca6f9b9c5873cb4238f7
nicole-esquer <nicole.esquer5@gmail.com>
2022-11-07 17:28:31 -0700

    testing for charities index page complete, small changes to more info button

--- spec/features/charity/index_spec.rb
+++ spec/features/charity/index_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Index Page for Charities' do
-  xit 'shows list of wildlife charities' do
-    visit charity_index_path
+  it 'shows list of wildlife charities', :vcr do
+    visit '/charities'
 
-    expect(page).to have_content("Name:")
+    click_on 'Wildlife Charities'
+
+    expect(page).to have_content('Wildlife Charities')
+    expect(page).to_not have_content('Animal Charities')
+    
+    expect(current_path).to eq('/charity')
+
+    expect(page).to have_content('Fungi Foundation')
+    expect(page).to have_button('More Info')
+  end
+
+
+  it 'shows list of animal charities', :vcr do
+    visit '/charities'
+
+    click_on 'Animal Charities'
+
+    expect(page).to have_content('Animal Charities')
+    expect(page).to_not have_content('Wildlife Charities')
+    
+    expect(current_path).to eq('/charity')
+
+    expect(page).to have_content('Fungi Foundation')
+    expect(page).to have_button('More Info')
   end
 end
