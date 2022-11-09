e0b515ff42271dbf07fcb7fc737beca4e6be3a9f
Nikky Rojas <rojas7313@gmail.com>
2022-11-09 11:35:14 -0700

    added method that creates an array of answers and shuffles

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,12 @@ class Answer < ApplicationRecord
   validates :correct, :incorrect1, :answer_type, presence: true
   validates_presence_of :incorrect2, :incorrect3, unless: -> { answer_type = 'boolean' }
   belongs_to :game
+
+  def shuffle(answers)
+    if answers.answer_type == "multiple"
+      return [answers.correct, answers.incorrect1, answers.incorrect2, answers.incorrect3].shuffle
+    else
+    return [answers.correct, answers.incorrect1].shuffle
+    end
+  end
 end
