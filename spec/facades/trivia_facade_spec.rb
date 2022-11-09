e912fc593b7b8bdee1225b3cb513c0ba4def41ec
Anna Marie Sterling <sterling.am@gmail.com>
2022-11-09 16:23:11 -0700

    refactor/remove files

--- spec/facades/trivia_facade_spec.rb
+++ spec/facades/trivia_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TriviaFacade do
   describe '.create_trivia_game', :vcr do
-    xit 'retrieves data for 10 trivia questions' do
+    it 'retrieves data for 10 trivia questions' do
       user = User.create!(name: 'Bob', email: 'bob@bob.com',uid: '1234567', token: 'abcdefg')
       trivia = TriviaFacade.create_trivia_game(user.id)
 
@@ -10,48 +10,18 @@ RSpec.describe TriviaFacade do
       expect(trivia.count).to eq(10)
 
       trivia.each do |section|
-        expect(section).to be_a Trivia
+        expect(section).to be_a Game
         expect(section.category).to eq('Animals')
-        expect(section.answers).to be_an Array
-        expect(section.correct_answer).to be_a String
         expect(section.difficulty).to be_a String
-        expect(section.incorrect_answers).to be_an Array
         expect(section.question).to be_a String
-        expect(section.type).to be_a String
-      end
-    end
-  end
-
-  describe '.create_tables', :vcr do
-    xit 'populates attributes to the Games table' do
-      game = TriviaFacade.create_game_table
-
-      expect(game).to be_an Array
-      game.each do |question|
-        expect(question.id).to be_an Integer
-        expect(question.difficulty).to be_a String
-        expect(question.question).to be_a String
-      end
-    end
-  end
-
-  describe '.create_answers', :vcr do
-    xit 'populates attributes to the Answers table' do
-      answers = TriviaFacade.create_answers
-
-      expect(answers).to be_an Array
-      answers.each do |answer|
-        expect(answer.id).to be_an Integer
-        expect(answer.game_id).to be_an Integer
-        expect(answer.answer_type).to be_a String
-        expect(answer.correct).to be_a String
-        expect(answer.incorrect1).to be_a String
-        if answer.answer_type == 'multiple'
-          expect(answer.incorrect2).to be_a String
-          expect(answer.incorrect3).to be_a String
-        else
-          expect(answer.incorrect2).to eq('')
-          expect(answer.incorrect3).to eq('')
+        expect(section.answer).to be_an Answer
+        expect(section.answer.correct).to be_a String
+        expect(section.answer.incorrect1).to be_an String
+        if section.answer.answer_type == 'multiple'
+          expect(section.answer.incorrect2).to be_a String
+          expect(section.answer.incorrect2).to_not eq('')
+          expect(section.answer.incorrect3).to be_a String
+          expect(section.answer.incorrect3).to_not eq('')
         end
       end
     end
